a71060fdfc2174ba2d7cc7f0bc3f5c27e86cc6ba
Phillip Kuhrt <mail@phi1010.com>
2025-06-17 18:21:20 +0200

    Fix compose volumes and debugging containers

--- debug-manage.sh
+++ debug-manage.sh
@@ -6,7 +6,7 @@ export ACTIVATE_DEBUG_MODE=active
 set -o allexport; source .env; set +o allexport
 COMPOSE="$COMPOSE -f docker-compose.yml -f docker-compose.debug.yml"
 . ./secrets.env
-$COMPOSE up opa db -d
+$COMPOSE up opa db openldap -d
 # Listen on 0.0.0.0 to allow access from inside docker container to this python instance.
 (cd src; pipenv run ./manage.py $*)
 
