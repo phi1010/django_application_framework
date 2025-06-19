8d6ed95d0503eedbcc064f73ad061f3c635b505e
Phillip Kuhrt <mail@phi1010.com>
2025-06-19 16:50:53 +0200

    Build python container before running a prod manage command

--- prod-manage.sh
+++ prod-manage.sh
@@ -6,5 +6,10 @@ export ACTIVATE_DEBUG_MODE=active
 set -o allexport; source .env; set +o allexport
 COMPOSE="$COMPOSE -f docker-compose.yml -f docker-compose.prod.yml"
 . ./secrets.env
+
+set -x
+echo "======== Building ========"
+$COMPOSE build python
+echo "======== Running Docker Compose Command ========"
 $COMPOSE run --rm python ./manage.py "$@"
 
