b868a8747ca037b0199e308ef0a7cc489d980f60
Phillip Kuhrt <mail@phi1010.com>
2025-06-19 16:47:54 +0200

    Fix shell injection and whitespace handling in commands

--- mqtt-dynsec.sh
+++ mqtt-dynsec.sh
@@ -6,10 +6,10 @@ COMPOSE="$COMPOSE -f docker-compose.yml -f docker-compose.prod.yml"
 . secrets.env
 
 if [ -z "$1" ] ; then
-  echo "Pass the command as \$*"
+  echo "Pass the command as \$@"
   exit 1
 fi
-$COMPOSE run --rm mqtt mosquitto_ctrl -h mqtt -u controller -P "${MQTT_PASSWD_CONTROLLER}" -p 1883 dynsec $*
+$COMPOSE run --rm mqtt mosquitto_ctrl -h mqtt -u controller -P "${MQTT_PASSWD_CONTROLLER}" -p 1883 dynsec "$@"
 
 
 
--- prod-manage.sh
+++ prod-manage.sh
@@ -6,5 +6,5 @@ export ACTIVATE_DEBUG_MODE=active
 set -o allexport; source .env; set +o allexport
 COMPOSE="$COMPOSE -f docker-compose.yml -f docker-compose.prod.yml"
 . ./secrets.env
-$COMPOSE run --rm python ./manage.py $*
+$COMPOSE run --rm python ./manage.py "$@"
 
