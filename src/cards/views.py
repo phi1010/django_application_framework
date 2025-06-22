0f425a04e23314db869400e6b8c4852cbdfddcd6
Phillip Kuhrt <mail@phi1010.com>
2025-06-22 18:03:15 +0200

    Mockup for locking cards.

--- src/cards/views.py
+++ src/cards/views.py
@@ -5,12 +5,10 @@ from django.contrib import messages
 
 @login_required(login_url='/') # TODO determine correct login URL
 def home(request):
-    messages.add_message(request, messages.INFO, "Hello world.")
-    messages.debug(request, "%s SQL statements were executed." % 1)
-    messages.info(request, "Three credits remain in your account.")
-    messages.success(request, "Profile details updated.")
-    messages.warning(request, "Your account expires in three days.")
-    messages.error(request, "Document deleted.")
+    messages.info(request, "Du hast lange nicht verwendete Karten. Bitte sperre oder lösche diese.")
+    messages.success(request, "Karte gesperrt.")
+    messages.warning(request, "Karte gelöscht.")
+    messages.error(request, "Karte konnte nicht gelesen werden. Bitte Karte erneut an das Lesegerät halten und Aktion wiederholen.")
     context = dict(title='Cards', site_title='ZAM Door', site_header='ZAM Door Commander',
                    messages=messages.get_messages(request),
                    )
